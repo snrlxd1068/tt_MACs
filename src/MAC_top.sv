c35c02de93111a3e2fc66d0b57fc4b42a9474b67
snrlxd1068 <jackgf6@gmail.com>
2026-04-28 07:02:39 -0400

    linear, lut and shift

--- src/MAC_top.sv
+++ src/MAC_top.sv
@@ -50,8 +50,10 @@ always_ff @(posedge clk) begin
     end
 end
 
-logic shift_working, log_working;
+logic linear_working, lut_working, shift_working, log_working;
+assign linear_working = !r_mode[1];
 assign log_working = r_mode[1];
+assign lut_working = (r_mode[1] && !r_mode[0]);
 assign shift_working = (r_mode[1] && r_mode[0]);
 
 logic [IDATAW-1:0] w_a_data, w_b_data; //data signals feeding to multiplier module
@@ -110,7 +112,14 @@ assign padded_result = {{PADBITS{1'b0}}, r_result};
 
 logic [OUTSELW-1:0] out_counter;
 
-assign w_result = shift_result;
+always_comb begin
+    case(r_mode)
+        2'b00: w_result = linear_result;
+        2'b10: w_result = lut_result;
+        2'b11: w_result = shift_result;
+        default: w_result = linear_result;
+    endcase
+end
 
 always_ff @(posedge clk) begin
     if(~rst_n) begin
@@ -130,10 +139,37 @@ end
 
 
 
+logic linear_mult_valid;
+logic [IDATAW*2-1:0] linear_mult_result;
+logic [RESULTW-1:0] linear_result;
+
+linear_mult #(
+    .IDATAW(IDATAW)
+) linear_mult_inst (
+    .clk(clk),
+    .rst_n(rst_n),
+    .in_a(w_a_data),
+    .in_b(w_b_data),
+    .i_valid(both_valid),
+    .working(linear_working),
+    .result(linear_mult_result),
+    .o_valid(linear_mult_valid)
+);
+linear_accum #(
+    .IDATAW(2*IDATAW),
+    .RESULTW(RESULTW)
+) linear_accum_inst (
+    .clk(clk),
+    .rst_n(rst_n),
+    .i_valid(linear_mult_valid),
+    .in_data(linear_mult_result),
+    .working(linear_working),
+    .result(linear_result)
+);
 
 logic log_mult_valid;
 logic [IDATAW:0] log_mult_result;
-logic [RESULTW-1:0] shift_result;
+logic [RESULTW-1:0] lut_result, shift_result;
 
 log_mult #(
     .IINTW(IINTW),
@@ -148,7 +184,21 @@ log_mult #(
     .result(log_mult_result),
     .o_valid(log_mult_valid)
 );
-
+log_accum_lut #(
+    .IINTW(IINTW + 1),
+    .IFRACW(IFRACW),
+    .RESULTINTW(RESULTINTW),
+    .RESULTFRACW(RESULTFRACW),
+    .DSTEP(DSTEP),
+    .NDSTEP(NDSTEP)
+) log_accum_lut_inst (
+    .clk(clk),
+    .rst_n(rst_n),
+    .i_valid(log_mult_valid),
+    .in_data(log_mult_result),
+    .working(lut_working),
+    .result(lut_result)
+);
 log_accum_shift #(
     .IINTW(IINTW + 1),
     .IFRACW(IFRACW),
